b5f9ac394ebf08cf8a478c19b626c1f6e2dbe61d
Hriday Jham <59745414+hridayjham@users.noreply.github.com>
2023-10-18 18:18:00 -0400

    Time: 213 ms (12.35%), Space: 23.3 MB (98.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,8 +4,8 @@ class Solution:
         answer = [1 for i in range(len(nums))]
         
         for i in range(1, len(nums)):
-            answer[i] *= nums[i-1] * answer[i] * answer[i-1]
-            
+            answer[i] *= nums[i-1] 
+            answer[i] *= answer[i-1]
             
         for i in range(len(nums) - 2, -1, -1):
             answer[i] *= post
