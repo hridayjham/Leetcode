01c2e287b8024f0bec82a94003065ca7065582ea
Hriday Jham <59745414+hridayjham@users.noreply.github.com>
2023-10-19 03:04:41 -0400

    Time: 1014 ms (68.27%), Space: 20.3 MB (89.81%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,20 +7,18 @@ class Solution:
             if i > 0 and nums[i] == nums[i - 1]:
                 i += 1
                 continue
-            l, r = i+1, len(nums) - 1
             
+            l, r = i + 1, len(nums) - 1
             while(l < r):
-                
                 threesum = a + nums[l] + nums[r]
-                if threesum > 0:
-                    r -= 1
-                elif threesum < 0:
+                if threesum < 0:
                     l += 1
+                elif threesum > 0:
+                    r -= 1
                 else:
                     res.append([a, nums[l], nums[r]])
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-                        
         return res
                                 
